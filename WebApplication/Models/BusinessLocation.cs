fd2ae76956507a2bb505f7945c2fd30a064bd6ff
Brian McKeiver <bmckeiver@bizstream.com>
2017-05-20 15:30:23 -0400

    FIxing zipcode bug.

--- WebApplication/Models/BusinessLocation.cs
+++ WebApplication/Models/BusinessLocation.cs
@@ -2,19 +2,6 @@
 
 namespace SocrataSodaNet.Models
 {
-    //public class BusinessLocation
-    //{
-    //    public string ID { get; set; }
-    //    public string Legal_Name { get; set; }
-    //    public string Doing_Business_As_Name { get; set; }
-    //    public string City { get; set; }
-    //    public string State { get; set; }
-    //    public string Zip_Code { get; set; }
-    //    public DateTime Date_Issued { get; set; }
-    //    public double? Latitude { get; set; }
-    //    public double? Longitude { get; set; }
-    //}
-
     public class BusinessLocation
     {
         public string ID { get; set; }
@@ -22,7 +9,7 @@ namespace SocrataSodaNet.Models
         public string DBA { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string Zip { get; set; }
+        public string Zip_Code { get; set; }
         public DateTime IssuedOn { get; set; }
         public double? Latitude { get; set; }
         public double? Longitude { get; set; }
